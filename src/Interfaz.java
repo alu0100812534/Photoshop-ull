a189c2506919900a3716ff13887f009e13d63f7c
alu0100845808 <alu0100845808@ull.edu.es>
2016-09-25 17:24:26 -0100

    Add files via upload
    Con más perry's que nunca pero bien colocados.

--- src/Interfaz.java
+++ src/Interfaz.java
@@ -1,96 +1,77 @@
 package perrysoft;
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.ArrayList;
+import java.io.PrintWriter;
 import java.util.Optional;
-
-import javax.imageio.ImageIO;
-import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.JDesktopPane;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Interfaz {
 	
 	private JFileChooser selector;
-	private BufferedImage imagenRetorno=null;
-	private Image img=null;
 	public JMenuItem load = new JMenuItem("Cargar");
+	public JMenuItem save = new JMenuItem("Guardar");
+	public JFrame ventana_ = new JFrame ("perrysoft");
+	public JDesktopPane DPane = new JDesktopPane();
+	public JPanel p1 = new JPanel ();
+	int perry=0;
+	
 	
 	Interfaz(){
-	    
-		
+	    	
 		CrearVentana();
 		
 	}
+	
 	void CrearVentana(){
-	
 		
-		JFrame ventana_ = new JFrame ("perrysofh");
 		ventana_.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-		ventana_.setSize (700, 700);
+		ventana_.setSize (1920, 1080);
 		ventana_.setVisible (true);
 		
-		JPanel p1 = new JPanel ();
 		p1.setLayout (new BorderLayout ());
 		
 		JMenuBar Mtop = new JMenuBar();
 		JMenu file = new JMenu("Archivo");
 		
 		file.add(load);
+		file.add(save);
 		Mtop.add(file);
 
-		
-
 		p1.add (Mtop,BorderLayout.NORTH);
+		p1.add (DPane,BorderLayout.CENTER);
 		ventana_.add (p1);
-		
-
-		
-		
+			
 	}
 	
-void CrearVentanaI(ArrayList<BufferedImage> imagenes){
+	void CrearVentanaI(BufferedImage imagenes){
 	
 		
-		JFrame ventana_ = new JFrame ("image");
-		ventana_.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-		ventana_.setSize (700, 700);
-		ventana_.setVisible (true);
-		
-		JPanel p1 = new JPanel ();
-		p1.setLayout (new BorderLayout ());
-		
-		JMenuBar Mtop = new JMenuBar();
-		JMenu file = new JMenu("Archivo");
-		
-		file.add(load);
-		Mtop.add(file);
+		JInternalFrame img_ = new JInternalFrame ("Imagen "+(++perry));
+		img_.setResizable(true);
+		img_.setClosable(true);
+		img_.pack();
+		img_.setDefaultCloseOperation (JFrame.DISPOSE_ON_CLOSE);
+		img_.setSize (imagenes.getWidth()+20,imagenes.getHeight()+40);
+		img_.setVisible (true);
+		img_.getContentPane().add(new JLabel(new ImageIcon(imagenes)));
+		img_.setVisible(true);
+		DPane.add(img_);
 
-		
-
-		p1.add (Mtop,BorderLayout.NORTH);
-		ventana_.add (p1);
-		
-
-		
-		
 	}
-	
-	
-	
-	
-	
+		
 	public Optional<File> openFileChooser() {
 		   selector = new JFileChooser();
 		   selector.setDialogTitle("Seleccionar Imagen");
@@ -104,46 +85,9 @@ void CrearVentanaI(ArrayList<BufferedImage> imagenes){
 	}
 	
 	
-	/*
-	
-    private void errorImagen(){
-        JOptionPane.showMessageDialog(null,"No se pudo cargar la imagen","Error",JOptionPane.ERROR_MESSAGE);
-    }
-
-	void ShowImage(){
-		JFrame ventana_ = new JFrame ("Imagen Cargada");
-		ventana_.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-		ventana_.setSize (200, 200);
-		ventana_.setVisible (true);
+	/*public void openFileSaver() {
 		
-		 JOptionPane.showMessageDialog(null,"CHORFIREEEEEEEEEEEEEE","Error",JOptionPane.ERROR_MESSAGE);
-		
+	          
 	}
-	///------>
-	public void abrirImagenLocal(){
-        if(this.openFileChooser()==true){
-            try {
-                imagenRetorno = ImageIO.read(this.selector.getSelectedFile());
-                if (imagenRetorno!=null){
-                   transformToImage();
-                   ShowImage();
-                   
-                }else{
-                	errorImagen();
-                }
-            } catch (Exception e) {
-                errorImagen();
-            }
-        }        
-    }
-	private void transformToImage(){
-		img = (Image)imagenRetorno;	
-	}
-	public Image get_Image(){
-		return img;
-	}
-	
-	public BufferedImage get_Image_Buffered(){
-		return imagenRetorno;
-	}*/
+	*/
 }
--- src/Modelo.java
+++ src/Modelo.java
@@ -19,15 +19,6 @@ public class Modelo {
 			try {
 				BufferedImage image = ImageIO.read(file); 
 			    imagenes.add(image);
-				
-			    //System.err.println("La imagen esta apestando png");
-				//imagenes.addElement(image);
-				//JFrame frame = new JFrame();
-				//frame.getContentPane().setLayout(new FlowLayout());
-				//frame.getContentPane().add(new JLabel(new ImageIcon(image)));
-				//frame.setVisible(true);
-				//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			
 			} catch (IOException e) {
 				System.err.println("La imagen esta apestando1");
 			}
@@ -48,14 +39,11 @@ public class Modelo {
 			System.err.println("No soportamos dicho formato");
         break;
 		
-		
-		
 		}
 	}
 	
-	 public ArrayList<BufferedImage> getImages(){
-	    return imagenes;
+	 public BufferedImage getImages(){
+	    return imagenes.get(imagenes.size()-1);
 	}
 	
-
 }
